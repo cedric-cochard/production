a5dd5debdff5bda8fbf1860d4f7093e8a5d00e4c
cedric-cochard <c.jc@live.fr>
2022-12-12 22:48:51 +0100

    color generator

---
+++ tp/JavaScript/4-color-generator/js/script.js
@@ -0,0 +1,14 @@
+const getColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+
+  const color = `rgb(${r}, ${g}, ${b})`;
+  console.log(color);
+
+  document.body.style.background = color;
+  document.body.innerHTML = `<h2>${color}</h2>`;
+};
+
+setInterval(getColor, 2000);
